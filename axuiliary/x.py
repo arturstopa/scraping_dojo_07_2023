b36fb8571cf66acfcc066f541759cceecaa8c343
Artur Stopa <arturstopa10@gmail.com>
2023-07-28 17:50:36 +0200

    scraping from js

---
+++ axuiliary/x.py
@@ -0,0 +1,24 @@
+{
+    "object": {
+        "property": [
+            {
+                "@name": "tags",
+                "array": {"string": ["change", "deep-thoughts", "thinking", "world"]},
+            },
+            {
+                "@name": "author",
+                "object": {
+                    "property": [
+                        {"@name": "name", "string": "Albert Einstein"},
+                        {"@name": "goodreads_link", "string": "/author/show/9810.Albert_Einstein",},
+                        {"@name": "slug", "string": "Albert-Einstein"},
+                    ]
+                },
+            },
+            {
+                "@name": "text",
+                "string": "“The world as we have created it is a process of our thinking. It cannot be changed without changing our thinking.”",
+            },
+        ]
+    }
+}
--- sigmoidal/sigmoidal/spiders/quote_spider.py
+++ sigmoidal/sigmoidal/spiders/quote_spider.py
@@ -1,7 +1,12 @@
 import scrapy
 from dotenv import load_dotenv
+import js2xml
+import lxml.etree
+import parsel
+import xmltodict
 
 import os
+from typing import List, Dict
 
 load_dotenv()
 
@@ -9,17 +14,43 @@ load_dotenv()
 class QuoteSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [os.getenv("INPUT_URL")]
-    start_urls = ["https://quotes.toscrape.com/tag/humor/"]
     proxy = os.getenv("PROXY")
     output_file = os.getenv("OUTPUT_FILE")
 
     def parse(self, response):
-        for quote in response.css("div.quote"):
-            yield {
-                "author": quote.xpath("span/small/text()").get(),
-                "text": quote.css("span.text::text").get(),
-                "tags": quote.xpath("div/a/text()").getall(),
-            }
+        js = response.css("script::text").get()
+        xml = lxml.etree.tostring(js2xml.parse(js), encoding="unicode")
+
+        selector = parsel.Selector(text=xml)
+        for quote in selector.xpath("//var[@name='data']/array/object").getall():
+            data: List[Dict] = (
+                xmltodict.parse(quote).get("object", dict()).get("property", list())
+            )
+            result = dict()
+            for property in data:
+                if property.get("@name", "") == "text":
+                    result["text"] = property.get("string", "")
+                if property.get("@name", "") == "author":
+                    author_properties: List[Dict] = property.get("object", dict()).get(
+                        "property", list()
+                    )
+                    name = [
+                        dictionary.get("string", "")
+                        for dictionary in author_properties
+                        if dictionary.get(
+                            "@name",
+                        )
+                        == "name"
+                    ][0]
+                    result["by"] = name
+                if property.get("@name") == "tags":
+                    try:
+                        tags = property.get("array", dict()).get("string", list())
+                    except AttributeError:
+                        tags = list()
+                    result["tags"] = tags if isinstance(tags, list) else [tags]
+
+            yield result
 
         next_page = response.css("li.next a::attr('href')").get()
         if next_page is not None:
